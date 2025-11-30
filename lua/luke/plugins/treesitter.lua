7f6773f00badc3ca5de77ce7975b5087a4e0198b
Sir-Mudkip <lukechappman@gmail.com>
2025-11-30 21:23:48 +0000

    Updated Nvim Config for LSP, Catppucin, and MD Tables

--- lua/luke/plugins/treesitter.lua
+++ lua/luke/plugins/treesitter.lua
@@ -1,8 +1,9 @@
-  return {
-  {
-	"nvim-treesitter/nvim-treesitter", 
-	branch = 'master', 
-	lazy = false, 
-	build = ":TSUpdate"  
-  },
+return {
+{ 
+    "nvim-treesitter/nvim-treesitter", 
+    branch = 'master',
+    lazy = false, 
+    build = ":TSUpdate"
+
+},
 }
